0cda369b5680326d33d461df1bbc1e5cf3d1466e
aribyui <ari22008@byui.edu>
2023-09-11 17:51:46 -0600

    C# Prep 2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string stringPercentage = Console.ReadLine();
+        int gradePercentage = int.Parse(stringPercentage);
+        string letter;
+
+        if (gradePercentage >= 90) 
+        {
+           letter = "A";
+        }
+        else if (gradePercentage >=80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >=70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >=60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Letter grade: {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed the course!");
+        }
+        else 
+        {
+            Console.WriteLine("We sorry, you did not pass the course.");
+        }
     }
 }
