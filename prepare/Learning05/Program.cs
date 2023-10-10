a4658db605c4995651cd491bc55f91672824e0cf
aribyui <ari22008@byui.edu>
2023-10-10 16:29:46 -0600

    C# Learning 05 finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -7,24 +7,31 @@ class Program
     {
         Console.Clear();
 
-        Square square = new Square("Red", 2);
-        // Console.WriteLine(square.GetColor());
-        // Console.WriteLine(square.GetArea());
+        // Square square = new Square("Red", 2);
+        // // Console.WriteLine(square.GetColor());
+        // // Console.WriteLine(square.GetArea());
 
-        Rectangle rectangle = new Rectangle("Yellow", 2, 8);
-        // Console.WriteLine(rectangle.GetColor());
-        // Console.WriteLine(rectangle.GetArea());
+        // Rectangle rectangle = new Rectangle("Yellow", 2, 8);
+        // // Console.WriteLine(rectangle.GetColor());
+        // // Console.WriteLine(rectangle.GetArea());
 
-        Circle circle = new Circle("Blue", 15);
-        // Console.WriteLine(circle.GetColor());
-        // Console.WriteLine(circle.GetArea());
+        // Circle circle = new Circle("Blue", 15);
+        // // Console.WriteLine(circle.GetColor());
+        // // Console.WriteLine(circle.GetArea());
+
+        // Console.WriteLine();
+
+        // List<Shape> shapes = new List<Shape>();
+        // shapes.Add(square);
+        // shapes.Add(rectangle);
+        // shapes.Add(circle);
 
         Console.WriteLine();
 
         List<Shape> shapes = new List<Shape>();
-        shapes.Add(square);
-        shapes.Add(rectangle);
-        shapes.Add(circle);
+        shapes.Add(new Square("Red", 2));
+        shapes.Add(new Rectangle("Yellow", 2, 8));
+        shapes.Add(new Circle("Blue", 15));
 
         foreach (Shape s in shapes)
         {
